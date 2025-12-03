72481f507a04c59df6f6e7e94492f0c730f58f40
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2025-12-03 13:18:07 +0700

    devops: fixed db initialization

--- backend/init.sql
+++ backend/init.sql
@@ -1,1 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS user_service_schema;
+CREATE SCHEMA IF NOT EXISTS board_service_schema;
+CREATE SCHEMA IF NOT EXISTS issue_service_schema;
+CREATE SCHEMA IF NOT EXISTS sprints_service_schema;
+CREATE SCHEMA IF NOT EXISTS dashboard_service_schema;
