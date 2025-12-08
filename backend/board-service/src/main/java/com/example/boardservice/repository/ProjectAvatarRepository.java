1306a23e660381937f5a33c06e1ab20997dd2159
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2025-12-09 01:47:55 +0700

    boardservice: create project avatar logic

--- backend/board-service/src/main/java/com/example/boardservice/repository/ProjectAvatarRepository.java
+++ backend/board-service/src/main/java/com/example/boardservice/repository/ProjectAvatarRepository.java
@@ -3,5 +3,8 @@ package com.example.boardservice.repository;
 import com.example.boardservice.dto.models.ProjectAvatar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectAvatarRepository extends JpaRepository<Long, ProjectAvatar> {
+import java.util.Optional;
+
+public interface ProjectAvatarRepository extends JpaRepository<ProjectAvatar, Long> {
+    Optional<ProjectAvatar> findByProjectId(Long projectId);
 }
